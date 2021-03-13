9f9d64d651591c00f2d9f8d39a3345caa292ded0
Miguel de Alba <malba@Air-de-Miguel.fritz.box>
2021-03-13 16:51:43 +0100

    Fix #9
    Fix #9

--- src/main/resources/data/initialdata/language_written_in.sql
+++ src/main/resources/data/initialdata/language_written_in.sql
@@ -54,3 +54,4 @@ INSERT INTO language_written_in (id, name) VALUES (54, '@Risk 5');
 INSERT INTO language_written_in (id, name) VALUES (55, '@Risk 6');
 INSERT INTO language_written_in (id, name) VALUES (56, '@Risk 7.5');
 INSERT INTO language_written_in (id, name) VALUES (57, 'Other');
+INSERT INTO language_written_in (id, name) VALUES (58, 'R 4');
