b3cb157f547293bf6619fbb41ce824e9871271f9
Miguel de Alba <miguelalba89@gmail.com>
2021-03-29 19:27:55 +0200

    Update LanguageRepository.java
    Use a variable binding mechanism to construct this query instead of concatenation.

--- src/main/java/de/bund/bfr/rakip/vocabularies/data/LanguageRepository.java
+++ src/main/java/de/bund/bfr/rakip/vocabularies/data/LanguageRepository.java
@@ -1,9 +1,6 @@
 package de.bund.bfr.rakip.vocabularies.data;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Optional;
 
@@ -19,10 +16,10 @@ public class LanguageRepository implements BasicRepository<Language> {
 
 	@Override
 	public Optional<Language> getById(int id) {
-
-		try {
-			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery("SELECT * FROM language WHERE id = " + id);
+		String query = "SELECT * FROM language WHERE id = ?";
+		try (PreparedStatement statement = connection.prepareStatement(query)) {
+			statement.setInt(1, id);
+			ResultSet resultSet = statement.executeQuery();
 
 			if (resultSet.next()) {
 				String code = resultSet.getString("code");
@@ -30,7 +27,9 @@ public class LanguageRepository implements BasicRepository<Language> {
 				return Optional.of(new Language(id, code, name));
 			}
 			return Optional.empty();
-		} catch (SQLException err) {
+
+		}
+		catch (SQLException err) {
 			return Optional.empty();
 		}
 	}
