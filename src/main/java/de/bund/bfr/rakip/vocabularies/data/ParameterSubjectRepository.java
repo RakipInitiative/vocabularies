6848f5589e258dab058fd99b5f26220914b5afb2
Miguel de Alba <miguelalba89@gmail.com>
2021-04-01 15:23:10 +0200

    Update ParameterSubjectRepository.java

--- src/main/java/de/bund/bfr/rakip/vocabularies/data/ParameterSubjectRepository.java
+++ src/main/java/de/bund/bfr/rakip/vocabularies/data/ParameterSubjectRepository.java
@@ -1,9 +1,6 @@
 package de.bund.bfr.rakip.vocabularies.data;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Optional;
 
@@ -20,13 +17,15 @@ public class ParameterSubjectRepository implements BasicRepository<ParameterSubj
 	@Override
 	public Optional<ParameterSubject> getById(int id) {
 
-		try {
-			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery("SELECT * FROM parameter_subject WHERE id = " + id);
+		String query = "SELECT * FROM parameter_subject WHERE id = ?";
+		try (PreparedStatement statement = connection.prepareStatement(query)) {
+			statement.setInt(1, id);
 
-			if (resultSet.next()) {
-				String name = resultSet.getString("name");
-				return Optional.of(new ParameterSubject(id, name));
+			try (ResultSet resultSet = statement.executeQuery()) {
+				if (resultSet.next()) {
+					String name = resultSet.getString("name");
+					return Optional.of(new ParameterSubject(id, name));
+				}
 			}
 			return Optional.empty();
 		} catch (SQLException err) {
