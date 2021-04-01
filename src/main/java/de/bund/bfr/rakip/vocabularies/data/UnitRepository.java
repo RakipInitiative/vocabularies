d37c82ed0558c181d1e3bd66fd61a2d60b064697
Miguel de Alba <miguelalba89@gmail.com>
2021-04-01 16:29:56 +0200

    Update UnitRepository.java

--- src/main/java/de/bund/bfr/rakip/vocabularies/data/UnitRepository.java
+++ src/main/java/de/bund/bfr/rakip/vocabularies/data/UnitRepository.java
@@ -1,14 +1,13 @@
 package de.bund.bfr.rakip.vocabularies.data;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Optional;
 
 import de.bund.bfr.rakip.vocabularies.domain.Unit;
 import de.bund.bfr.rakip.vocabularies.domain.UnitCategory;
+
+import javax.xml.transform.Result;
 
 public class UnitRepository implements BasicRepository<Unit> {
 
@@ -23,23 +22,23 @@ public class UnitRepository implements BasicRepository<Unit> {
 	@Override
 	public Optional<Unit> getById(int id) {
 
-		try {
-			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery("SELECT * FROM unit WHERE id = " + id);
+		String query = "SELECT * FROM unit WHERE id = ?";
+		try (PreparedStatement statement = connection.prepareStatement(query)) {
+			statement.setInt(1, id);
+			try (ResultSet resultSet = statement.executeQuery()) {
+				if (resultSet.next()) {
+					String name = resultSet.getString("name");
+					String ssd = resultSet.getString("ssd");
+					String comment = resultSet.getString("comment");
+					int categoryId = resultSet.getInt("category_id");
 
-			if (resultSet.next()) {
-				String name = resultSet.getString("name");
-				String ssd = resultSet.getString("ssd");
-				String comment = resultSet.getString("comment");
-				int categoryId = resultSet.getInt("category_id");
-
-				Optional<UnitCategory> category = categoryRepository.getById(categoryId);
-				if (category.isPresent()) {
-					return Optional.of(new Unit(id, name, ssd, comment, category.get()));
+					Optional<UnitCategory> category = categoryRepository.getById(categoryId);
+					if (category.isPresent()) {
+						return Optional.of(new Unit(id, name, ssd, comment, category.get()));
+					}
 				}
 			}
 			return Optional.empty();
-
 		} catch (SQLException err) {
 			return Optional.empty();
 		}
