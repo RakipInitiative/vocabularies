742f0107c5062766c4a45e0cba087e5ebd7cc17a
Miguel de Alba <miguelalba89@gmail.com>
2021-04-01 15:52:44 +0200

    Update PopulationRepository.java

--- src/main/java/de/bund/bfr/rakip/vocabularies/data/PopulationRepository.java
+++ src/main/java/de/bund/bfr/rakip/vocabularies/data/PopulationRepository.java
@@ -1,9 +1,6 @@
 package de.bund.bfr.rakip.vocabularies.data;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Optional;
 
@@ -20,14 +17,16 @@ public class PopulationRepository implements BasicRepository<Population> {
 	@Override
 	public Optional<Population> getById(int id) {
 
-		try {
-			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery("SELECT * FROM population WHERE id = " + id);
+		String query = "SELECT * FROM population WHERE id = ?";
+		try (PreparedStatement statement = connection.prepareStatement(query)) {
+			statement.setInt(1, id);
 
-			if (resultSet.next()) {
-				String name = resultSet.getString("name");
-				String foodon = resultSet.getString("foodon");
-				return Optional.of(new Population(id, name, foodon));
+			try (ResultSet resultSet = statement.executeQuery()) {
+				if (resultSet.next()) {
+					String name = resultSet.getString("name");
+					String foodon = resultSet.getString("foodon");
+					return Optional.of(new Population(id, name, foodon));
+				}
 			}
 			return Optional.empty();
 		} catch (SQLException err) {
